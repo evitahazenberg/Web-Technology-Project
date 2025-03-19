b7a258950af5f8e5fdc6611d42ee6ddb8d04e5f8
Daphne <d.f.meekers@student.rug.nl>
2025-03-19 13:04:34 +0100

    added javascript for accordion, sourced from w3 schools. Enhanced the looks a bit

--- js/accordion.js
+++ js/accordion.js
@@ -1,1 +1,16 @@
-// Accordion function
+var acc = document.getElementsByClassName("accordion-header");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// Source: https://www.w3schools.com/howto/howto_js_accordion.asp
