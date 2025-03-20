de02cf0c564f581ea1088e545d730ab1b168c35b
Daphne <d.f.meekers@student.rug.nl>
2025-03-20 10:31:25 +0100

    Added the missing pages, not the content of the pages and enhanced the looks

--- js/accordion.js
+++ js/accordion.js
@@ -2,12 +2,19 @@ var acc = document.getElementsByClassName("accordion-header");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  // Add a click event listener to each accordion header
   acc[i].addEventListener("click", function() {
+    
+    // Add or remove the active class on the clicked header
     this.classList.toggle("active");
+    // Get the next element sibling, which is the accordion content
     var panel = this.nextElementSibling;
+
+    // If the panel has a max-height, remove it
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
+      // Otherwise, set the max-height to the scroll height of the panel
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
